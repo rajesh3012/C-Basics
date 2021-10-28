e235835b90a12a6d508fb79dcad0787f288184a1
rajesh3012 <92074755+rajesh3012@users.noreply.github.com>
2021-10-28 10:50:21 +0530

    Add files via upload

---
+++ trails 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaratoin
+	float  rajesh_a;
+	int rajesh_b,rajesh_c;
+	char total_rajesh;
+	
+	//initization
+	rajesh_a = 5.8 ;
+	rajesh_b = 10 ;
+	rajesh_c = 45 ;
+	
+	total_rajesh = fail
+	//output
+	printf("\n%t%f rajesh_a \n%t%d rajesh_b \n%t%d rajesh_c \n%t%c total_rajesh",rajesh_a,rajesh_b,rajesh_c,total_rajesh);
+}
