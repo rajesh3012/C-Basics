f02d96e8a2e3ac787ab6f4340ce6f3f67b20ffdd
rajesh3012 <92074755+rajesh3012@users.noreply.github.com>
2021-10-28 09:56:09 +0530

    Add files via upload

---
+++ adding student marks.c
@@ -0,0 +1,23 @@
+	/* c program  adding student marks*/
+#include<stdio.h>
+void main()
+
+{
+	  //variable declaratoin
+	  int tel ,hin ,max, phy, chy ,ss, total;
+	  
+	  //intizatoin
+	  tel = 50;
+	  hin = 49;
+	  max = 69;
+	  phy = 70;
+	  chy = 69;
+	  ss  = 77;
+	  //calculation
+	  total = tel+hin+max+phy+chy+ss;
+	  //output
+	  printf("the total marks of the student \n	tel\t%d \n	hin\t%d \n	max\t%d \n	phy\t%d \n	chy\t%d \n	ss\t%d \n	total\t%d",tel,hin,max,phy,chy,ss,total);
+	  
+	  
+}
+
